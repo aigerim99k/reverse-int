734b3cf893c3ce73dce07f44951433da4b3fc61c
aigerim99k <aigerim.99k@gmaol.com>
2021-03-15 12:52:17 +0500

    init: solve reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+if(n<0){
+    n=-1*n;
+}
+var arr=[];
+do{
+    var mod=n%10;
+    arr.push(mod);
+    n=Math.floor(n/10);
+}
+while(n>0);
+return arr.join('');
 }
